60bda4ea181f2ce0c37edefe1437d23514500983
UlVc <ulvc@ciencias.unam.mx>
2022-02-27 14:56:03 -0600

    Translated into english

--- src/dominio.py
+++ src/dominio.py
@@ -1,38 +1,24 @@
-def _lista_a_dict(l):
-    """ Pone a todos los elementos de la lista en un diccionario cuya
-    llave es el nombre del objeto.
-    Sólo sirve para listas de objetos con atributo 'nombre'.
-    """
-    d = {}
-    for o in l:
-        d[o.nombre] = o
-    return d
-
-class Dominio:
-    """ Clase para definir el dominio, o espacio de estados en el cual se plantearán problemas de planeación. """
-    def __init__(self, nombre, tipos, predicados, acciones):
+class Domain:
+    """Representation of the domain."""
+    def __init__(self, name, types, predicates, actions):
         """
-        Inicializa un dominio
-        :param nombre:
-        :param tipos:
-        :param predicados:
-        :param acciones:
+        :param name: Name of the domain.
+        :param types: Types of the domain.
+        :param predicates: Predicates of the domain.
+        :param actions: Actions of the domain.
         """
-        self.nombre = nombre
-        self.tipos = tipos
-        self.predicados = predicados
-        self.acciones = acciones
-
-        self._predicados = _lista_a_dict(predicados)
+        self.name = name
+        self.types = types
+        self.predicates = predicates
+        self.actions = actions
 
     def __str__(self):
-        dic = {'name':          self.nombre,
-               'types':         "\n        ".join(self.tipos),
-               'predicates':    "\n        ".join(str(p) for p in self.predicados),
-               'actions':       "\n    ".join(str(a) for a in self.acciones)
+        dic = {'name':          self.name,
+               'types':         "\n        ".join(self.types),
+               'predicates':    "\n        ".join(str(p) for p in self.predicates),
+               'actions':       "\n    ".join(str(a) for a in self.actions)
                }
-        return """
-(define (domain {name})
+        return """(define (domain {name})
     (:requirements :strips :typing)
     (:types
         {types}
@@ -42,10 +28,4 @@ class Dominio:
     )
     {actions}
 )
-        """.format(**dic)
-
-    def declaración(self, nombre):
-        """ 
-        Devuelve la declaración del predicado con el nombre indicado.
-        """
-        return self._predicados[nombre]
+""".format(**dic)
