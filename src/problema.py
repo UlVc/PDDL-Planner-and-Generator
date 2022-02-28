1f8a7d965e4e579abfdb88f67ec29d1b4ab3ce81
UlVc <ulvc@ciencias.unam.mx>
2022-02-27 20:02:08 -0600

    Translated into english

--- src/problema.py
+++ src/problema.py
@@ -1,36 +1,37 @@
-class Problema:
-    """ Definicion de un problema en un dominio particular. """
-    def __init__(self, nombre, dominio, objetos, predicados, predicados_meta):
+class Problem:
+    """Representation of a problem."""
+    def __init__(self, name, domain, objects, predicates, goal):
         """
-        Problema de planeación en una instancia del dominio.
-        :param nombre: nombre del problema
-        :param dominio: referencia al objeto con la descripción genérica del dominio
-        :param objetos: lista de objetos existentes en el dominio, con sus tipos
-        :param predicados: lista de predicados con sus variables aterrizadas, indicando qué cosas son verdaderas en el
-               estado inicial.  Todo aquello que no esté listado es falso.
-        :param predicados_meta: lista de predicados con sus variables aterrizadas, indicando aquellas cosas que deben
-               ser verdaderas al final.  Para indicar que algo debe ser falso, el predicado debe ser negativo.
+        :param name: Name of the problem.
+        :param domain: Domain in which the problem will be defined.
+        :param objects: Types of the domain.
+        :param predicates: List of predicates indicating that they are true. Anything else is considered to be false.
+        :param goal: List of predicates. It indicates the predicates that need to be true at the end.
         """
-        self.nombre = nombre
-        self.dominio = dominio # ref a objeto Dominio
-        d_objetos = {}
-        for objeto in objetos:
-            if objeto.tipo not in d_objetos:
-                d_objetos[objeto.tipo] = [objeto]
+        self.name = name
+        self.domain = domain
+        d_objects = {}
+
+        for object in objects:
+            if object.type not in d_objects:
+                d_objects[object.type] = [object]
             else:
-                d_objetos[objeto.tipo].append(objeto)
-        self.d_objetos = d_objetos
-        self.estado = predicados
-        self.meta = predicados_meta
+                d_objects[object.type].append(object)
+
+        self.d_objects = d_objects
+        self.state = predicates
+        self.goal = goal
 
     def __str__(self):
-        dic = {'name':          self.nombre,
-               'domain_name':   self.dominio.nombre,
-               'objects':       "\n      ".join(" ".join(o.nombre for o in self.d_objetos[tipo]) + " - " + tipo for tipo in self.d_objetos),
-               'init':          "\n      ".join(str(p) for p in self.estado),
-               'goal':          "\n      ".join(str(p) for p in self.meta)}
-        if len(self.meta) >= 2:
+        dic = {'name':          self.name,
+               'domain_name':   self.domain.name,
+               'objects':       "\n      ".join(" ".join(o.name for o in self.d_objects[type]) + " - " + type for type in self.d_objects),
+               'init':          "\n      ".join(str(p) for p in self.state),
+               'goal':          "\n      ".join(str(p) for p in self.goal)}
+
+        if len(self.goal) >= 2:
             dic['goal'] = "(and " + dic['goal'] + ")"
+
         return """(define (problem {name}
     (:domain {domain_name})
     (:objects
@@ -40,4 +41,4 @@ class Problema:
     (:goal
       {goal})
 )
-        """.format(**dic)
+""".format(**dic)
